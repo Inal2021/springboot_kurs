ede20af382e0db9db95dd724fd20bf77716e8a4a
Hakan Inal <89166736+Inal2021@users.noreply.github.com>
2021-12-10 10:45:29 +0100

    Initial commit

---
+++ src/main/java/com/techpro/springboot_kurs/StudentBean01.java
@@ -0,0 +1,51 @@
+package com.techpro.springboot_kurs;
+
+import org.springframework.stereotype.Component;
+
+@Component  // Bu anotation kullandiginiz class dan obje olusturup, IOC Container icersine koyar. ==> Loose coupling
+			//Yani new StudentBean01() yerine bunu yazmis gibi oluyoruz. ==> Tight Coupling
+public class StudentBean01 implements StudentInterface {
+	//SpringBoot da Bean obje demektir. IOC Container icindeki tüm objelere Bean denir.
+	private String name;
+    private int age;
+    private String id;
+    public StudentBean01() {
+        System.out.println("Constructor without parameter is used");
+    }
+    public StudentBean01(String name, int age, String id) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    @Override
+    public String toString() {
+        return "StudentBean01: name=" + name + ", age=" + age + ", id=" + id;
+    }
+    
+	@Override
+	public String study() {
+		
+		return "StudentBean01 classindan geliyorum...";
+	}
+  
+	
+}
---
+++ src/main/java/com/techpro/springboot_kurs/StudentBean01Controller.java
@@ -0,0 +1,111 @@
+package com.techpro.springboot_kurs;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Controller     // Bu anotation ile Springboot bu klasin Controller Class oldugunu anlar.
+@RestController
+public class StudentBean01Controller {  //Bu klasta Müsterinin istekleri kontrol edilecek. Get mi Put mu Delete mi???
+
+//	@RequestMapping(method=RequestMethod.GET, path="/getRequest")
+//	@ResponseBody  //Methodum return ettigi datayi görmek icin bu annotation kullanilir.
+//	public String getMethod1() {
+//		return "Get Request Method1 calistirildi...";
+//	}
+	
+	@GetMapping(path="/getString")  // GetRequestleri bu maplerle eslestir demis olduk
+	public String getMethod1() {
+		return "Get Request Method1 calistirildi...";
+	}
+	
+	
+	//Tight coupling
+	@GetMapping(path="/getObject")
+	public StudentBean01 getMethod2() {
+		return new StudentBean01("Ali Can", 13, "AC202113");
+	}
+	
+	//Loose coupling
+	@Autowired//@Autowired annotation’i IOC Container’dan objeyi alir ve
+    // “s” container’inin icine koyar. StudentBean01 s = new StudentBean01() deki “=” operatorune benzer
+	StudentBean01 s;
+	
+	@GetMapping(path="/getObjectLoose")
+	public StudentBean01 getMethod3() {
+		
+		s.setName("Ali Can");
+		s.setAge(13);
+		s.setId("AC202113");
+		
+		return s;
+	}
+	
+	
+	// Tight Coupling
+		@GetMapping(path="/getObjectParametreli1/{school}")
+		public StudentBean01 getMethod4(@PathVariable String school) {
+			
+			return new StudentBean01("Veli Han", 35, String.format("VH2021%s35", school)) ;
+								
+		}
+	
+	
+	
+	
+	// Loose Coupling
+	@GetMapping(path="/getObjectParametreli2/{school}")
+	public StudentBean01 getMethod5(@PathVariable String school) {
+		s.setName("Veli Han");
+		s.setAge(35);
+		s.setId(String.format("VH2021%s35", school)); //%s datanin gelmesi icin yer tutucu
+		
+		return s;
+		
+	}
+	
+	
+	//Tight Coupling
+	@GetMapping(path="/getObjectList")
+	public List<StudentBean01> getMethod6(){
+		
+		
+		return List.of(
+				new StudentBean01("Veli Han", 35, "VH202135"),
+				new StudentBean01("Ayse Kan", 23, "VH202123"),
+				new StudentBean01("Talha Tan", 46, "VH202146")
+				);
+	}
+	
+	
+	//Loose coupling
+	@Autowired
+	StudentBean02 t;   // Class ismini yazarak IOC Containerdan istediginiz objeyi alabilirsiniz
+	
+	@GetMapping(path="/getStudy")
+	public String getMethod7() {
+		return t.study();
+	}
+	
+	
+	//Loose coupling
+		@Autowired      // Buradan Java hata verir. Cünkü Interface altinda hangi class istedigimizi bilemez
+		@Qualifier(value="studentBean02")  // Autowired anotation normalde data type a bakar.
+											// @Qualifier(value="") yazarak isme bakmasini saglariz.
+		StudentInterface u;   
+		
+		@GetMapping(path="/getInterface") 
+		public String getMethod8() {
+			return t.study();
+		}
+	
+	
+}
